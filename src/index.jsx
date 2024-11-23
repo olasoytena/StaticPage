b3a5644af1f5950c8658431a26ab19b84e05a60a
olasoytena <olasoytena@gmail.com>
2024-11-23 02:06:14 -0700

    Simplified files

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import {createRoot} from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <ul>
+        <li>I want to make money.</li>
+        <li>React is a very popular JS library.</li>
+        <li>I like programming.</li>
+    </ul>
+)
