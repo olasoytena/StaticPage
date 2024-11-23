4b0024961876706ab13d5437a7de48415fc5da80
olasoytena <olasoytena@gmail.com>
2024-11-23 02:22:11 -0700

    Creating components

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,14 @@
 import {createRoot} from "react-dom/client"
 
 const root = createRoot(document.getElementById("root"))
+const reactElement = <h1>Hello from JSX!</h1>
 
+function MainContent() {
+    return(
+        <h1>Hello World!</h1>
+    )
+    
+}
 root.render(
-    <ul>
-        <li>I want to make money.</li>
-        <li>React is a very popular JS library.</li>
-        <li>I like programming.</li>
-    </ul>
+    <MainContent />
 )
